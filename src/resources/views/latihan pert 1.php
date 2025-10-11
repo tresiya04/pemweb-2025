d7d441554b06ca14408e29195948b65202a3c479
root <root@LAPTOP-EFDT1HU8.>
2025-10-11 14:33:19 +0700

    🔥 fresh from the oven

---
+++ src/resources/views/latihan pert 1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<h1>DUNIA STRAWBERRY</h1>
+<h2>Sweet Charlie is an American-bred June-bearing strawberry variety known for its early, sweet, and high-quality fruit</h2>
+<h3>The Oso Grande is a strawberry variety originating from California, known for its large size, dense, firm flesh, and a balanced sweet taste.</h3>
+<h4>Jenis strawberry california tumbuh subur di daerah Berastagi, Kabupaten Karo, Sumatera Utara. Uniknya, jenis ini dapat tumbuh di dataran rendah.</h4>
+
+<p>Selamat datang di Dunia Strawberry
+</p>
+<h3>Dunia Strawberry yummy</h3>
+<ul>
+<li>Strawberry</li>
+<form onsubmit="handleSubmit(event)">
+            <label for="nama buah">Name:</label>
+            <input type="text" id="name" name="name" required><br>
+            
+            <label for="">Jenis:</label>
+            <input type="jenis" id="jenis" name="jenis" required><br>
+            
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" rows="4" required></textarea><br>
+            
+             <button type="submit">Submit</button>
+</form>
+<div id="response-message">
+ 
+</ul>
+<img src="assets/image.png" alt="Sample Image" class="sample-image">
+<i><a href="https://www.sayurbox.com/blog/10-jenis-strawberry">Strawberry</a></i>
+</div>
+</html>
